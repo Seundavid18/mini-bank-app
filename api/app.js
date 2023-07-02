56c52be0295d20f1be3c92b7c6439f22739baad5
Seun-David <oyewumidavid84@gmail.com>
2023-07-02 21:27:40 +0100

    updated production script

--- api/app.js
+++ api/app.js
@@ -37,10 +37,10 @@ const router = require("./src/router/userrouter")
 app.use(cors())
 app.use("/api",router)
 
-app.use(express.static(path.join(__dirname, "/client/build")))
+app.use(express.static("./client/build"))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 })
 
 
