9bba659faa46a009f9548d9ecd96de4ea31dbed8
Seundavid18 <oyewumidavid84@gmail.com>
2024-04-20 10:03:31 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -23,25 +23,25 @@ const storage = multer.diskStorage({
     filename:(req, file, cb) => {
         cb(null, req.body.name)
     },
-})
+});
 
 const upload = multer({storage: storage})
 app.post('/api/upload', upload.single("file"), (req, res) => {
     res.status(200).json('File has been uploaded')
-})
+});
 
 
 
-const router = require("./src/router/userrouter")
+const router = require("./src/router/userrouter");
 
-app.use(cors())
-app.use("/api",router)
+app.use(cors());
+app.use("/api",router);
 
-app.use(express.static("/home/site/wwwroot/client/build"))
+app.use(express.static("./client/build"));
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-})
+});
 
 // app.use(express.static(path.join(__dirname, "/client/build")))
 
