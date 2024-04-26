f591ae8d6f0cbcefc5f5759877219287ee0ff047
Seundavid18 <oyewumidavid84@gmail.com>
2024-04-26 09:15:35 +0100

    updated validation

--- client/src/utils.js
+++ client/src/utils.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
     baseURL: "https://seunbankapp.azurewebsites.net/api"
-    //baseURL: "http://localhost:5000/api"
+    //baseURL: "http://localhost:4000/api"
 })
